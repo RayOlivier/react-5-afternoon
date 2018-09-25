2d31ee164992e3125684350703108098eaafab86
Ray Olivier <rayolivier@outlook.com>
2018-09-24 21:30:11 -0500

    base requirements met

--- src/components/WizardTwo/WizardTwo.js
+++ src/components/WizardTwo/WizardTwo.js
@@ -1,21 +1,44 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import { connect } from "react-redux"
+import { updateCity } from "../../ducks/reducer"
 
 class WizardTwo extends Component {
-    render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">
-            
-                <p>In what city will the property be located?</p><br />
-                    
-                        <input placeholder="city name" type="text" onChange={this.props.updateCity}/>
-                
-                    <Link to="/wThree"><button className="wTwo-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props)
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>In what city will the property be located?</p>
+          <br />
+
+          <input
+            placeholder="city name"
+            type="text"
+            onChange={(e) => this.props.updateCity(e.target.value)}
+          />
+
+          <Link to="/wThree">
+            <button className="wTwo-btn"> Next </button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default WizardTwo;
+// function mapStateToProps(state) {
+//   const { city } = state
+//   return { city }
+// }
+// } //this is the solution in the project but it doesn't work
+
+const mapStateToProps = (state) => {
+  console.log(state)
+  return state
+}
+
+export default connect(
+  mapStateToProps,
+  { updateCity }
+)(WizardTwo)
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,237 @@
-function reducer(){
+const initialState = {
+  loanType: "Home Purchase",
+  propertyType: "Single Family Home",
+  city: "",
+  propToBeUsedOn: "",
+  found: false,
+  realEstateAgent: "false",
+  cost: 0,
+  downPayment: 0,
+  credit: "",
+  history: "",
+  addressOne: "",
+  addressTwo: "",
+  addressThree: "",
+  firstName: "",
+  lastName: "",
+  email: ""
+}
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE"
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE"
+const UPDATE_PROP = "UPDATE_PROP"
+const UPDATE_CITY = "UPDATE_CITY"
+const UPDATE_FOUND = "UPDATE_FOUND"
+const UPDATE_AGENT = "UPDATE_AGENT"
+const UPDATE_COST = "UPDATE_COST"
+const UPDATE_DOWN_PAYMENT = "UPDATE_DOWN_PAYMENT"
+const UPDATE_CREDIT = "UPDATE_CREDIT"
+const UPDATE_HISTORY = "UPDATE_HISTORY"
+const UPDATE_ADDRESS_1 = "UPDATE_ADDRESS_1"
+const UPDATE_ADDRESS_2 = "UPDATE_ADDRESS_2"
+const UPDATE_ADDRESS_3 = "UPDATE_ADDRESS_3"
+const UPDATE_FIRST = "UPDATE_FIRST"
+const UPDATE_LAST = "UPDATE_LAST"
+const UPDATE_EMAIL = "UPDATE_EMAIL"
 
+export function updateEmail(x) {
+  return {
+    type: UPDATE_EMAIL,
+    payload: x
+  }
 }
 
-export default reducer;
+export function updateLast(x) {
+  return {
+    type: UPDATE_LAST,
+    payload: x
+  }
+}
+
+export function updateFirst(x) {
+  return {
+    type: UPDATE_FIRST,
+    payload: x
+  }
+}
+
+export function updateAddress3(x) {
+  return {
+    type: UPDATE_ADDRESS_3,
+    payload: x
+  }
+}
+
+export function updateAddress2(x) {
+  return {
+    type: UPDATE_ADDRESS_2,
+    payload: x
+  }
+}
+
+export function updateAddress1(x) {
+  return {
+    type: UPDATE_ADDRESS_1,
+    payload: x
+  }
+}
+
+export function updateHistory(x) {
+  return {
+    type: UPDATE_HISTORY,
+    payload: x
+  }
+}
+
+export function updateCredit(x) {
+  return {
+    type: UPDATE_CREDIT,
+    payload: x
+  }
+}
+
+export function updateDownPayment(x) {
+  return {
+    type: UPDATE_DOWN_PAYMENT,
+    payload: x
+  }
+}
+
+export function updateCost(input) {
+  return {
+    type: UPDATE_COST,
+    payload: input
+  }
+}
+
+export function updateAgent(input) {
+  return {
+    type: UPDATE_AGENT,
+    payload: input
+  }
+}
+export function updateLoanType(loanType) {
+  return {
+    type: UPDATE_LOAN_TYPE,
+    payload: loanType
+  }
+}
+
+export function updatePropertyType(property) {
+  return {
+    type: UPDATE_PROPERTY_TYPE,
+    payload: property
+  }
+}
+
+export function updateCity(input) {
+  return {
+    type: UPDATE_CITY,
+    payload: input
+  }
+}
+
+export function updateProp(prop) {
+  return {
+    type: UPDATE_PROP,
+    payload: prop
+  }
+}
+
+export function updateFound(found) {
+  return {
+    type: UPDATE_FOUND,
+    payload: found
+  }
+}
+
+function reducer(state = initialState, action) {
+  console.log("REDUCER HIT: Action ->", action)
+
+  switch (action.type) {
+    case UPDATE_EMAIL:
+      return {
+        ...state,
+        email: action.payload
+      }
+    case UPDATE_FIRST:
+      return {
+        ...state,
+        firstName: action.payload
+      }
+    case UPDATE_LAST:
+      return {
+        ...state,
+        lastName: action.payload
+      }
+    case UPDATE_ADDRESS_1:
+      return {
+        ...state,
+        addressOne: action.payload
+      }
+    case UPDATE_ADDRESS_2:
+      return {
+        ...state,
+        addressTwo: action.payload
+      }
+    case UPDATE_ADDRESS_3:
+      return {
+        ...state,
+        addressThree: action.payload
+      }
+    case UPDATE_HISTORY:
+      return {
+        ...state,
+        history: action.payload
+      }
+    case UPDATE_CREDIT:
+      return {
+        ...state,
+        credit: action.payload
+      }
+    case UPDATE_COST:
+      return {
+        ...state,
+        cost: action.payload
+      }
+    case UPDATE_DOWN_PAYMENT:
+      return {
+        ...state,
+        downPayment: action.state
+      }
+    case UPDATE_AGENT:
+      return {
+        ...state,
+        realEstateAgent: action.payload
+      }
+    case UPDATE_LOAN_TYPE:
+      return {
+        ...state,
+        loanType: action.payload
+      }
+    case UPDATE_PROPERTY_TYPE:
+      return {
+        ...state,
+        propertyType: action.payload
+      }
+
+    case UPDATE_CITY:
+      return {
+        ...state,
+        city: action.payload
+      }
+    case UPDATE_PROP:
+      return {
+        ...state,
+        propToBeUsedOn: action.payload
+      }
+    case UPDATE_FOUND:
+      return {
+        ...state,
+        found: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
